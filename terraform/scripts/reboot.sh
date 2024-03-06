ee49e2f1310c10c264577e063d266a3c9c653b08
Kian <kian@ocf.berkeley.edu>
2024-03-05 17:47:45 -0800

    Update domain in scripts

--- terraform/scripts/reboot.sh
+++ terraform/scripts/reboot.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 if [ "$#" = 1 ]; then
   username=$1
   dir=$(dirname "$0")
-  ssh -i "$dir/../../data/decal_root" -o "StrictHostKeyChecking=no" "root@$username.decal.xcf.sh" "reboot now"
+  ssh -i "$dir/../../data/decal_root" -o "StrictHostKeyChecking=no" "root@$username.decal.ocfhosted.com" "reboot now"
 else
 	echo "Usage: $0 <ocfusername>"
 fi
--- terraform/scripts/reset_password.sh
+++ terraform/scripts/reset_password.sh
@@ -5,7 +5,7 @@ if [ "$#" = 1 ]; then
   username=$1
   dir=$(dirname "$0")
   read -p "New password for $username: " newpw
-  { echo passwd $username; sleep 3; echo $newpw; sleep 1; echo $newpw; echo passwd -e $username; echo exit; } | ssh -i "$dir/../../data/decal_root" -o "StrictHostKeyChecking=no" "root@$username.decal.xcf.sh"
+  { echo passwd $username; sleep 3; echo $newpw; sleep 1; echo $newpw; echo passwd -e $username; echo exit; } | ssh -i "$dir/../../data/decal_root" -o "StrictHostKeyChecking=no" "root@$username.decal.ocfhosted.com"
 else
 	echo "Usage: $0 <ocfusername>"
 fi
